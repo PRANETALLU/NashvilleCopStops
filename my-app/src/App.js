f9f4cb7d6b4f008dc2a90bd2454e53a2d19e16d7
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-31 19:36:27 -0400

    routes are setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,23 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './CSS/Header.css'
-import './CSS/Main.css'
-import './CSS/Main.css'
+import './CSS/Header.css';
+import './CSS/Main.css';
+import './CSS/Main.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
