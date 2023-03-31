0993155129d33d56191df0c6eb8aaff670a61875
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-31 16:41:43 -0400

    header done

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,9 +4,28 @@ function Header() {
     return (
         <div className="Header">
             <p className="headerLogoText">Nashville Police Data</p>
-            <Button>Graph<br/>Visualization</Button>
-            <Button>Geo<br/>Visualization</Button>
-            <Button>Database<br/>Search</Button>
+            <Button 
+            sx=
+            {{mt: -1, mr: 3, 
+            fontWeight: 700, fontSize: 23, 
+            color: "white", 
+            ":hover":{color: "#D9D9D9"}}}>
+                Graph<br/>Visualization
+            </Button>
+            <Button 
+            sx={{mt: -1, mr: 3, 
+            fontWeight: 700, fontSize: 23, 
+            color: "white", 
+            ":hover":{color: "#D9D9D9"}}}>
+                Geo<br/>Visualization
+            </Button>
+            <Button 
+            sx={{mt: -1, mr: 3, 
+            fontWeight: 700, fontSize: 23, 
+            color: "white", 
+            ":hover":{color: "#D9D9D9"}}}>
+                Database<br/>Search
+            </Button>
         </div>
     );
 }
