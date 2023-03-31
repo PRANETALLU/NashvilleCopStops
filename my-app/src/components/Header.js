0241845fa6361ff999e62b17a759ce448f27c818
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-31 19:23:51 -0400

    spacing set

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -12,6 +12,7 @@ function Header() {
             ":hover":{color: "#D9D9D9"}}}>
                 Graph<br/>Visualization
             </Button>
+
             <Button 
             sx={{mt: -1, mr: 3, 
             fontWeight: 700, fontSize: 23, 
@@ -19,6 +20,7 @@ function Header() {
             ":hover":{color: "#D9D9D9"}}}>
                 Geo<br/>Visualization
             </Button>
+            
             <Button 
             sx={{mt: -1, mr: 3, 
             fontWeight: 700, fontSize: 23, 
