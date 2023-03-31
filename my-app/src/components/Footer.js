0f54a5b046589d33df2ec7d2319272d2d6d49dce
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-31 18:11:11 -0400

    github image import and hyperlink

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,7 +3,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 function Footer() {
     return (
         <div className="Footer">
-            <GitHubIcon sx={{width: 45, height: 45}}></GitHubIcon>
+            <a href="https://github.com/PRANETALLU/NashvilleCopStops" target="_blank"><GitHubIcon sx={{width: 45, height: 45}}></GitHubIcon></a>
         </div>
     );
 }
