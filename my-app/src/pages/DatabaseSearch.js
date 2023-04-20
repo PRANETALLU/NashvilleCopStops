ff69a1480f2d6c9f2e8eb6e588fd47a7da25364d
pedro-camargo11 <pedro.manzi.camargo@gmail.com>
2023-04-20 14:11:23 -0400

    add use state to the frontend for database search feature

--- my-app/src/pages/DatabaseSearch.js
+++ my-app/src/pages/DatabaseSearch.js
@@ -9,8 +9,41 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function DatabaseSearch() {
+
+    const [stopInfo, setStopInfo] = useState({
+        day: "",
+        month: "",
+        hour: "",
+        min: "",
+        time: "",
+        location: "",
+        precinct: "",
+        reportingArea: "",
+        zone: "",
+        officerID: "",
+        reasonForStop: ""
+    });
+    
+    const handleStopInfoChange = (event) => {
+        const { name, value } = event.target;
+        setStopInfo({ ...stopInfo, [name]: value });
+        console.log(stopInfo);
+    }
+
+    const [subjectInfo, setSubjectInfo] = useState({
+        age: "",
+        race: "",
+        sex: "",
+    });
+
+    const handleSubjectInfoChange = (event) => {
+        const { name, value } = event.target;
+        setSubjectInfo({ ...subjectInfo, [name]: value });
+    }
+
     return (
         <div className="DatabaseSearch">
             <Box sx={{
@@ -31,66 +64,87 @@ function DatabaseSearch() {
                     <Typography fontSize={15} marginTop={1}>
                         Date
                     </Typography>
-                    <TextField label="Day" size="small" variant="outlined"
+                    <TextField label="Day" name="day" size="small" variant="outlined"
+                        value = {stopInfo.day}
+                        onChange = {handleStopInfoChange}
                         sx={{ marginLeft: 1.5, width: "90px" }} />
                     <Typography fontSize={15} marginLeft={1} marginTop={1}>
                         -
                     </Typography>
-                    <TextField label="Month" size="small" variant="outlined"
+                    <TextField label="Month" name ="month" size="small" variant="outlined"
+                        value = {stopInfo.month}
+                        onChange = {handleStopInfoChange}
                         sx={{ marginLeft: 1, width: "150px" }} />
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Time
                     </Typography>
-                    <TextField label="Hour" size="small" variant="outlined"
+                    <TextField label="Hour" name="hour"  size="small" variant="outlined"
+                        value = {stopInfo.hour}
+                        onChange = {handleStopInfoChange}
                         sx={{ marginLeft: 1.5, width: "90px" }} />
                     <Typography fontSize={15} marginLeft={1} marginTop={1}>
-                        -
+                         -
                     </Typography>
-                    <TextField label="Min" size="small" variant="outlined"
+                    <TextField label="Min" name="min"  size="small" variant="outlined"
+                        value = {stopInfo.min}
+                        onChange = {handleStopInfoChange}
                         sx={{ marginLeft: 1, width: "150px" }} />
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Location
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="location" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.location}
+                        onChange = {handleStopInfoChange}>
+                        <MenuItem value="Street">Street</MenuItem>
                     </Select>
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Precinct
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="precinct" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.precinct}
+                        onChange = {handleStopInfoChange}>
                     </Select>
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Reporting Area
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="reportingArea" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.reportingArea}
+                        onChange = {handleStopInfoChange}>
                     </Select>
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Zone
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="zone" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.zone}
+                        onChange = {handleStopInfoChange}>
                     </Select>
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Officer ID (Hashed)
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="officerID" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.officerID}
+                        onChange = {handleStopInfoChange}>
                     </Select>
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Reason For Stop
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="reasonForStop" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {stopInfo.reasonForStop}
+                        onChange = {handleStopInfoChange}>
                     </Select>
                 </Stack>
             </Box>
@@ -113,14 +167,18 @@ function DatabaseSearch() {
                     <Typography fontSize={15} marginTop={1}>
                         Age
                     </Typography>
-                    <TextField label="Years" size="small" variant="outlined"
+                    <TextField label="Years" name="age" size="small" variant="outlined"
+                        value = {subjectInfo.age}
+                        onChange = {handleSubjectInfoChange}
                         sx={{ marginLeft: 1.5, width: "90px" }} />
                 </Stack>
                 <Stack direction="row" marginLeft={2} marginTop={2}>
                     <Typography fontSize={15} marginTop={1}>
                         Race
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="race" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {setSubjectInfo.race}
+                        onChange={handleSubjectInfoChange}>
                         <MenuItem value="White">White</MenuItem>
                         <MenuItem value="Black">Black</MenuItem>
                         <MenuItem value="Hispanic">Hispanic</MenuItem>
@@ -131,7 +189,9 @@ function DatabaseSearch() {
                     <Typography fontSize={15} marginTop={1}>
                         Sex
                     </Typography>
-                    <Select size="small" sx={{ width: "150px", marginLeft: 1.5 }}>
+                    <Select size="small" name="sex" sx={{ width: "150px", marginLeft: 1.5 }}
+                        value = {subjectInfo.sex}
+                        onChange = {handleSubjectInfoChange}>
                         <MenuItem value="Male">Male</MenuItem>
                         <MenuItem value="Female">Female</MenuItem>
                     </Select>
