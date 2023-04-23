731f213dd3391aa52aa43cfbc4efc95f763e3767
pedro-camargo11 <pedro.manzi.camargo@gmail.com>
2023-04-23 18:11:28 -0400

    add graphs to graphVisualization page

--- my-app/src/pages/GraphVisualization.js
+++ my-app/src/pages/GraphVisualization.js
@@ -13,7 +13,7 @@ import averageAgeTopPrecinct from '../images/graphs/average_age_top_precincts.pn
 import cumulativePercentageOfOfficersVsStops from '../images/graphs/cumulative_percentage_of_officers_vs_stops_2012.png'
 import maleFemaleOutcomesOfStop from '../images/graphs/male_female_outcomes_of_stops.png'
 import percentageOfStopsByOutcome from '../images/graphs/percentage_of_stops_by_outcome_per_year.png'
-import percentageOfStopsByRace from '../images/graphs/search_contraband_gender_comparison.png'
+import searchContrabandGenderComparison from '../images/graphs/search_contraband_gender_comparison.png'
 import seachesPer100Drivers from '../images/graphs/searches_per_100_drivers.png'
 import topPrecinctsByPercentageOfStops from '../images/graphs/top_3_precincts_by_percentage_of_total_stops_per_year.png'
 
@@ -68,15 +68,14 @@ function GraphVisualization() {
                         <Item>
                             Description
                             & 
-                        Findings</Item>
+                        Findings 
+                        Add description here </Item>
                     </Grid>
                     <Grid item  md={5}>
-                        <Item>Bar graph:
-Find highest 3 precincts (by # of stops) each year and determine % of total stops that they accounted for that year</Item>
+                        <Item><img src = {topPrecinctsByPercentageOfStops} className = "" ></img></Item>
                     </Grid>
                     <Grid item  md={5}>
-                        <Item>Bar graph:
-Out of all stops in a year, what percentage of stops resulted in each of the three outcomes (warning, citation, arrest)? Look for differences over the years</Item>
+                        <Item><img src={percentageOfStopsByOutcome} className =""></img></Item>
                     </Grid>
                     <Grid item  md={5}>
                         <Item><img src={Graph1Nash} className = "graphImg"></img></Item>
@@ -110,8 +109,7 @@ Out of all stops in a year, what percentage of stops resulted in each of the thr
 Over the time of data collection, is there a difference in the average number of stops per race made during different hours of the day?</Item>
                     </Grid>
                     <Grid item xs={6}>
-                        <Item>Multi line graph:
-For each rate, what is the difference between the number of searches conducted for every 100 drivers, and how has this changed over the years?</Item>
+                        <Item><img src = {seachesPer100Drivers} className = ""></img></Item>
                     </Grid>
                 </Grid>
             </Box>
@@ -135,12 +133,10 @@ For each rate, what is the difference between the number of searches conducted f
                         Findings</Item>
                     </Grid>
                     <Grid item xs={6}>
-                        <Item>Double line graph:
-Out of all stops in a year with outcome of warning or citation, what percentage of stops, for each gender, resulted in a warning vs a citation. (x-axis: year; y-axis: % warning outcome)</Item>
+                        <Item><img src = {maleFemaleOutcomesOfStop} className = "graphImg" ></img></Item>
                     </Grid>
                     <Grid item xs={6}>
-                        <Item>Bar graph:
-For each gender, what are the percentage of searches conducted conducted result in contraband, drugs, or weapons, and how have these numbers changed over time?</Item>
+                        <Item><img src = {searchContrabandGenderComparison} className="graphImg"></img></Item>
                     </Grid>
                     <Grid item xs={6}>
                         <Item> <img src = {averageAgeTopPrecinct} className = "graphImg"></img></Item>
