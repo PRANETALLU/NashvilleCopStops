65182869f6e0cca7abd8f11f76ec9b2aa59127e3
Cyrus Estavillo <cyrus.estavillo@gmail.com>
2023-04-24 22:58:59 -0400

    Added descriptions to graph except for race

--- my-app/src/pages/GraphVisualization.js
+++ my-app/src/pages/GraphVisualization.js
@@ -38,7 +38,7 @@ function GraphVisualization() {
             <Typography gutterBottom variant="h6" component="div"
                 style={{
                     fontWeight: 'bold', textAlign: 'left',
-                    marginTop: 50, marginLeft: 115
+                    marginTop: 50, marginLeft: 115, fontSize: 22
                 }}>
                 Police Stops: Frequency and Outcome
             </Typography>
@@ -47,9 +47,26 @@ function GraphVisualization() {
                 <Grid container rowSpacing={0.5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                     <Grid item md={5}>
                         <Typography >
-                            Description
-                            & 
-                        Findings 
+                            <br></br>
+                            We start by using complex queries to analyze the data pertaining to our data's observed
+                            officers, precincts, and overall stop makeup. The top right graph helps our readers
+                            to identify an interesting trend: certain precincts consistently account for a large volume
+                            of Nashville's crime. Precincts 3 and 5 consistently appear in the top 3 precincts for total
+                            stops per year. This is interesting because precinct 3 is located in the heart of downtown
+                            Nashville, and precinct 5 is located in the heart of East Nashville. These precincts are
+                            located in the heart of Nashville's nightlife, and it is interesting to see that they
+                            consistently account for a large volume of Nashville's crime.
+                            <br></br>
+                            The bottom left graph shows the percentage of stops by outcome per year. This graph is interesting because it
+                            shows that the percentage of stops that resulting in a citation has been steadily climbing in the 
+                            last few recorded years, with less warnings being given out per stop. 
+                            <br></br>
+                            The bottom right graph shows the cumulative percentage of officers vs stops. What is notable here is that the top 10% of 
+                            officers (in terms of number of stops in the year 2012) accounted for nearly 50% of the total stops
+                            in Nashville. Clearly, this shows that a small percentage of Nashville's police force are responsible
+                            for the majority of community interactions in this context.
+
+
                          </Typography>
                     </Grid>
                     <Grid item  md={5}>
@@ -103,15 +120,31 @@ Over the time of data collection, is there a difference in the average number of
                 }}>
                 Gender, Age, and Inequality
             </Typography>
-
-
-            <Box sx={{ width: '100%', marginBottom: 4 }}>
+            
+            <Box sx={{ width: '100%', marginLeft: 4}}>
                 <Grid container rowSpacing={0.5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={6}>
-                        <Item>
-                            Description
-                            & 
-                        Findings</Item>
+                    <Grid item md={5}>
+                        <Typography >
+                            <br></br>
+                            We end by analyzing the relationship between police stops, gender, and age.
+                            First, let us explore how gender affects the outcome of a police stop. The top right graph
+                            shows that, over all of the years of data collection, women are consistently more likely
+                            to receive a warning than their male counterparts. Furthermore, men are signficantly more
+                            likely to be involved in an arrest during their stop with a poice officer than their female 
+                            peers. However, observing this without context is not enough to draw conclusions about the
+                            nature of police stops in Nashville.
+                            <br></br>
+                            To provide us with a more complete picture, we look to the bottom left graph. This graph
+                            analyzes the nature of police search outcomes in the context of gender. As the graph shows,
+                            men are significantly more likely to have contraband of any kind, including drugs and weapons.
+                            Luckily for the safety of Nashville civilians, and likely to the relief of Nashville police,
+                            less searches are resulting in weapons found over the years.
+                            <br></br>
+                            We conclude by observing how age plays into police stops. As the reader can see, the majority
+                            of drivers stopped are between the ages of 36 and 38. Furthermore, the average age of drivers
+                            stopped has been increasing in some precincts while decreasing in others, likely due to a difference
+                            in what is offered in the area. 
+                         </Typography>
                     </Grid>
                     <Grid item xs={6}>
                         <Item><img src = {maleFemaleOutcomesOfStop} className = "graphImg" ></img></Item>
@@ -124,6 +157,8 @@ Over the time of data collection, is there a difference in the average number of
                     </Grid>
                 </Grid>
             </Box>
+
+            
         </div>
     );
 }
